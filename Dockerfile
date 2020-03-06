1282814c5a58b95d24df5197a9657e6d93454ee7
quericy <quericy@live.com>
2020-03-06 14:44:12 +0800

    modify():change clone

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM richarvey/nginx-php-fpm
 
 COPY docker-entrypoint.sh /
 
+RUN git clone https://github.com/SomeBottle/OneIndex.git /var/www/html && \
+    ssh-keygen -A
+
 # env params
-ENV GIT_REPO=github.com/SomeBottle/OneIndex/
-ENV GIT_EMAIL=git@test.com
-ENV GIT_USERNAME=git_user
+ENV REMOVE_FILES=0
 
 # Persistent config file and cache
 VOLUME [ "/var/www/html/config", "/var/www/html/cache" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 # crontab
 if [ -z $DISABLE_CRON ];then
-    REFRESH_TOKEN=${REFRESH_TOKEN:-"0 * * * *"}
-    REFRESH_CACHE=${REFRESH_CACHE:-"*/10 * * * *"}
+    REFRESH_TOKEN=${REFRESH_TOKEN:-"0/60 * * * *"}
+    REFRESH_CACHE=${REFRESH_CACHE:-"*/30 * * * *"}
     rm -rf /tmp/cron.`whoami`
     echo "${REFRESH_TOKEN} php /var/www/html/one.php token:refresh" >> /tmp/cron.`whoami`
     echo "${REFRESH_CACHE} php /var/www/html/one.php cache:refresh" >> /tmp/cron.`whoami`
